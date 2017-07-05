05319e283b68c371bb36564bb7296d894ad885cc
Darwel Quintero <darwel34@gmail.com>
2017-07-05 09:14:24 -0430

    se creo el archivo que ejecuta el server de node haciendo uso de express

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'pug');
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.listen(3000, function(err){
+  if(err){
+    console.log('Hubo un error'), process.exit(1);
+  }
+  console.log('Plazigram escuchando en el puerto 3000');
+});
