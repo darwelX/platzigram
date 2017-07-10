d2caaab7444cd4a188993379050b9058d3cf9799
Darwel Quintero <darwel34@gmail.com>
2017-07-10 09:23:14 -0430

    se agregaron los archivos estaticos

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'pug');
+//se le dice que la carpeta esta disponible de manera estatica
+app.use(express.static('public'));
 app.get('/', function(req, res){
   res.render('index');
 });
