193b34765b4b84f453b1729aa10cbc4c570863f1
Darwel Quintero <darwel34@gmail.com>
2017-07-17 11:25:16 -0430

    se eliminaron las rutas y se añadieron las plantillas creadas para cada ruta eliminada

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
+// utilizada para el manejo de rutas
 var page = require('page');
-
-var main = document.getElementById('main-container');
-
-page('/', function (ctx, next){
-  main.innerHTML="Home <a href='/signup'>Signup</a>";
-});
-
-page('/signup', function(ctx, next){
-  main.innerHTML="Signup <a href='/'>Home</a>";
-});
-
+require('./homepage');
+require('./signup');
+require('./signin');
 page.start();
