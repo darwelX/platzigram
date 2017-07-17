ec37803a7ed8c1a1fb1ea743351673a6be17c843
Darwel Quintero <darwel34@gmail.com>
2017-07-17 11:14:37 -0430

    se agrego el listener para estar a la escuhca de errores en los fuentes javascript

--- Gulpfile.js
+++ Gulpfile.js
@@ -31,6 +31,7 @@ function compile(watch){
     bundle
       .transform(babel, {presets: ['es2015']})
       .bundle()
+      .on('error', function (err){ console.log(err); this.emit('end')})
       .pipe(source('index.js'))
       .pipe(rename('app.js'))
       .pipe(gulp.dest('public'));
