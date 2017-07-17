e0f48da244975109af0df945d18d840e13b91163
Darwel Quintero <darwel34@gmail.com>
2017-07-17 11:26:18 -0430

    se creo el index del home page

---
+++ src/homepage/index.js
@@ -0,0 +1,14 @@
+var page = require('page');
+// utilizada para el uso de templates
+var yo  = require('yo-yo');
+// utilizada como helper para usar en conjunto con yo-yo
+var empty = require('empty-element');
+// contiene el template html de la pagina
+var template = require('./template');
+var title = require('title');
+
+page('/', function(ctx, next){
+  title('Platzigram');
+  var main = document.getElementById('main-container');
+  empty(main).appendChild(template);
+});
