99125208e36fdb620ab1758aca21c25559cbbf5e
Darwel Quintero <darwel34@gmail.com>
2017-07-19 20:56:07 -0430

    se agrego el manejo de fechas y  las funciones que manejan los me gusta tambien se refactorizo la creacion de la plantilla

--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,20 +1,41 @@
 var yo = require('yo-yo');
+var moment = require('moment');
 
-module.exports = function(pic){
-  return yo`<div class="card">
-    <div class="card-image">
-      <img class="activator" src="${pic.url}">
-    </div>
-    <div class="card-content">
-      <a href="/user/${pic.user.username}" class="card-title">
-        <img src="${pic.user.avatar}" class="avatar"/>
-        <span class="username">${pic.user.username}</span>
-      </a>
-      <small class="right time">Hace 1 día</small>
-      <p>
-        <a class="left" href="#"><i class="fa fa-heart-o"></i></a>
-        <span class="left likes">${pic.likes} me gusta</span>
-      </p>
-    </div>
-  </div>`;
+module.exports = function pictureCard(pic){
+  var el;
+
+  function render(picture){
+      return yo`<div class="card ${picture.liked ? 'liked':''}">
+      <div class="card-image">
+        <img class="activator" src="${picture.url}">
+      </div>
+      <div class="card-content">
+        <a href="/user/${picture.user.username}" class="card-title">
+          <img src="${picture.user.avatar}" class="avatar"/>
+          <span class="username">${picture.user.username}</span>
+        </a>
+        <small class="right time">${moment(picture.createdAt).fromNow()}</small>
+        <p>
+          <a class="left" href="#" onclick=${like.bind(null, true)}><i class="fa fa-heart-o"></i></a>
+          <a class="left" href="#" onclick=${like.bind(null, false)}><i class="fa fa-heart"></i></a>
+          <span class="left likes">${picture.likes} me gusta</span>
+        </p>
+      </div>
+    </div>`;
+  }
+
+  // like.bind(null, true)
+  // este llamado se realiza de esta forma ya que de la manera tradicional
+  // la funcion se ejecutaria infinitamente, al usar bind nos retorna una referencia 
+  // a la funcion pero con el parametro cambiado segun lo necesite
+  function like(liked){
+    pic.liked = liked;
+    pic.likes += liked ? 1 : -1;
+    var newEl = render(pic);
+    yo.update(el, newEl);
+    return false;
+  }
+
+  el = render(pic);
+  return el;
 }
