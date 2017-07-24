b4f7374326641525e6efac524749cde635bc63b2
Darwel Quintero <darwel34@gmail.com>
2017-07-24 14:44:50 -0430

    se agregaron los mensajes para el idioma ingles

---
+++ src/translate/en-US.js
@@ -0,0 +1,21 @@
+module.exports = {
+  likes: '{likes, plural, ' +
+            '=0 {no likes.}' +
+            '=1 { # like.}' +
+            'other { # likes.}}',
+  'logout': 'Logout',
+  'english': 'English',
+  'spanish': 'Spanish',
+  'signup.subheading': 'Signup to watch your friends\' pictures studying at Platzi',
+  'signup.facebook': 'Signin with Facebook',
+  'signup': 'Signup',
+  'email': 'Email',
+  'username':  'Username',
+  'fullname': 'Fullname',
+  'password': 'Password',
+  'signup.call-to-action': 'Signup',
+  'signup.have-account': '¿Alredy have an account?',
+  'signin': 'Signin',
+  'signin.not-have-account': '¿Don\'t have an account?',
+  'language': 'Language'            
+}
