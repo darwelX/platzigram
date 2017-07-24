7274a86b3bb673ed10765e8112923a36795637e5
Darwel Quintero <darwel34@gmail.com>
2017-07-24 14:30:38 -0430

    se agrego una ruta para simular una ruta de api y que retorne un objeto json con las pictures

--- server.js
+++ server.js
@@ -13,7 +13,33 @@ app.get('/signup', function(req, res){
 app.get('/signin', function(req, res){
   res.render('index', {title: 'Platzigram - Signin'});
 });
-
+app.get('/api/pictures', function (req, res){
+  var pictures = [
+    {
+      user: {
+        username: 'darwelX',
+        avatar: 'https://pickaface.net/assets/images/slides/slide2.png'
+      },
+      url: 'http://materializecss.com/images/office.jpg',
+      likes: 0,
+      liked: false,
+      createdAt: +new Date()
+    },
+    {
+      user: {
+        username: 'darwelX',
+        avatar: 'https://pickaface.net/assets/images/slides/slide2.png'
+      },
+      url: 'http://materializecss.com/images/office.jpg',
+      likes: 1,
+      liked: false,
+      createdAt: +new Date().setDate(new Date().getDate() - 10)
+    }    
+  ];
+  setTimeout(function (){
+    res.send(pictures);
+  }, 2000);
+});
 app.listen(3000, function(err){
   if(err){
     console.log('Hubo un error'), process.exit(1);
