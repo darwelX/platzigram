1fd00fbfcc2191562f91ce0293ce53dffd359a49
Darwel Quintero <darwel34@gmail.com>
2017-07-24 14:43:57 -0430

    se agrego la internacionalización

--- src/signin/template.js
+++ src/signin/template.js
@@ -1,5 +1,6 @@
 var yo = require('yo-yo');
 var landing = require('../landing');
+var translate = require('../translate');
 
 var signinForm = yo`
         <div class="col s12 m7">
@@ -8,14 +9,14 @@ var signinForm = yo`
               <h1 class="platzigram">Platzigram</h1>
               <form class="signup-form">
                 <div class="section">
-                  <a href="" class="btn btn-fb hide-on-small-only"><i class="fa fa-facebook-official"></i>Iniciar sesión con Facebook</a>
-                  <a href="" class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
+                  <a href="" class="btn btn-fb hide-on-small-only"><i class="fa fa-facebook-official"></i>${translate.message('signup.facebook')}</a>
+                  <a href="" class="btn btn-fb hide-on-med-and-up">${translate.message('signup')}</a>
                 </div>
                 <div class="divider"></div>
                 <div class="section">
-                  <input type="text" name="user_name" placeholder="Nombre de usuario"/>
-                  <input type="password" name="password" placeholder="Contraseña"/>
-                  <button class="btn waves-effect waves-light btn-signup" type="submit">Iniciar Sesión</button>
+                  <input type="text" name="user_name" placeholder="${translate.message('username')}"/>
+                  <input type="password" name="password" placeholder="${translate.message('password')}"/>
+                  <button class="btn waves-effect waves-light btn-signup" type="submit">${translate.message('signup')}</button>
                 </div>
               </form>
             </div>
@@ -23,7 +24,7 @@ var signinForm = yo`
 
           <div class="row">
             <div class="login-box">
-              ¿No tienes una cuenta? <a href="/signup">Registrate</a>
+              ${translate.message('signin.not-have-account')} <a href="/signup">${translate.message('signup.call-to-action')}</a>
             </div>
           </div>
         </div>`;
