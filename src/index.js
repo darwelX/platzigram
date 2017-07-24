6cdf1c91aeb0fc65081230fbd4b552bdcb4481c8
Darwel Quintero <darwel34@gmail.com>
2017-07-24 14:31:11 -0430

    se agrego el llamado a footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // utilizada para el manejo de rutas
 var page = require('page');
 require('moment/locale/es');
-
 require('./homepage');
 require('./signup');
 require('./signin');
+require('./footer');
 page.start();
