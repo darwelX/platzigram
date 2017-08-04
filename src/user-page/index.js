97c1805f82305ebf2e61b85d045874b7b828c656
Darwel Quintero <darwel34@gmail.com>
2017-08-04 15:08:12 -0430

    se agrego la ruta para mostrar la picture y se añadio el spiner en los llamdos de las rutas

--- src/user-page/index.js
+++ src/user-page/index.js
@@ -3,13 +3,33 @@ import header from '../header';
 import title from 'title';
 import empty from 'empty-element';
 import template from './template';
+import spiner from '../spiner';
 
-page('/:username', header, loadUser, function(ctx, next){
+page('/:username', header, spiner, loadUser, function(ctx, next){
   var main = document.getElementById('main-container');
   title(`Platzigram - ${ctx.params.username}`);
   empty(main).appendChild(template(ctx.user));
 });
 
+page('/:username/:id', header, spiner, loadUser, function(ctx, next){
+  var main = document.getElementById('main-container');
+  title(`Platzigram - ${ctx.params.username}`);
+  empty(main).appendChild(template(ctx.user));
+  $('.modal').modal();
+  $('.modal').modal({
+    ready: function() { // Callback for Modal open. Modal and trigger parameters available.
+        alert("Ready");
+    },
+    complete: function(){
+      console.log('cerro');
+      let path = `/${ctx.params.username}`;
+      page(path);
+    }
+  });
+  $(`#modal${ctx.params.id}`).modal('open');
+  //let path = `/${ctx.params.username}`;
+  //page(path);
+});
 async function loadUser(ctx, next){
   try {
     ctx.user = await fetch(`/api/user/${ctx.params.username}`).then( res => res.json());
