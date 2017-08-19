9255901abd74912dd81a3f83c23ba3725317236a
Darwel Quintero <darwel34@gmail.com>
2017-08-19 10:27:46 -0430

    se agrego la funcionalidad para la toma de fotografias

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -10,6 +10,8 @@ var request = require('superagent');
 var header = require('../header');
 var axios = require('axios');
 var spiner = require('../spiner');
+var Webcams = require('webcamjs');
+var picture = require('../picture-card');
 
 
 /**
@@ -21,6 +23,82 @@ page('/', header, spiner, loadPictures, function(ctx, next){
   title('Platzigram');
   var main = document.getElementById('main-container');
   empty(main).appendChild(template(ctx.pictures));
+
+  const uploadButton = $('#uploadButton');
+  const cancelButton = $('#cancelPicture');
+  const shootButton = $('#shoot');
+  const previewPicture = $('#picture-preview');
+  const camaraInput = $('#camara-input');
+
+  $('.modal-trigger').modal({
+    ready: function (modal, trigger){
+      Webcams.attach('#camara-input');
+    },
+    complete: function (){
+      Webcams.reset();
+    }
+  });
+  $('#launch').modal({
+    ready: function (modal, trigger){
+      console.log('listo');
+    },
+    complete: function (){
+      console.log('adios');
+      Webcams.reset();
+    }
+  });
+  $('#launch').click(function(eve){
+    eve.preventDefault();
+    $('.modal').modal();
+    Webcams.on( 'load', function() {
+		  console.log('cargado');
+    });
+    Webcams.set({
+		width: 320,
+		height: 240
+    });
+    Webcams.attach('#camara-input');
+    shootButton.click((eve) => {
+      Webcams.snap( (data_uri) => {
+        previewPicture.html(`<img src="${data_uri}"/>`);
+        previewPicture.removeClass('hide');
+        uploadButton.removeClass('hide');
+        cancelButton.removeClass('hide');
+        shootButton.addClass('hide');
+        camaraInput.addClass('hide');
+        ///uploadButton.off('click');
+        uploadButton.click(function (eve){
+          const pic = {
+            url: data_uri,
+            likes: 0,
+            liked: false,
+            createAt: +new Date(),
+            user: {
+              username: 'darwelX',
+              avatar: 'https://pickaface.net/assets/images/slides/slide2.png'
+            }
+          }
+
+          $('#picture-card').prepend(picture(pic));
+          $('.modal').modal('close');
+          reset();
+          Webcams.reset();
+        });
+      });
+    });
+
+    cancelButton.click((eve) => {
+      reset();
+    });
+    
+    function reset(){
+      previewPicture.addClass('hide');
+      uploadButton.addClass('hide');
+      cancelButton.addClass('hide');
+      shootButton.removeClass('hide');
+      camaraInput.removeClass('hide');
+    }
+  });
 });
 
 /**
