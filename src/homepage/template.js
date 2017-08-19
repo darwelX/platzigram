a855c4bb2b2dad70f80fbb8f04ad48329e9b6127
Darwel Quintero <darwel34@gmail.com>
2017-08-19 10:28:22 -0430

    se agrego un id para mejor manejo del dom

--- src/homepage/template.js
+++ src/homepage/template.js
@@ -7,11 +7,31 @@ var request = require('superagent');
 module.exports = function (pictures){
   var el = yo`
   <div class="container timeline">
+    <div id="modal1" class="modal modal-fixed-footer center-align">
+      <div class="modal-content">
+        <div class="camara-picture" id="camara-input"></div>
+        <div class="camara-picture hide" id="picture-preview"></div>
+      </div>
+      <div class="modal-footer center-align">
+        <button class="waves-effect waves-light btn" id="shoot">
+          <i class="fa fa-camera"></i>
+        </button>
+        <button class="waves-effect waves-light btn cyan hide" id="uploadButton">
+          <i class="fa fa-cloud-upload"></i>
+        </button>              
+        <button class="waves-effect waves-light btn red hide" id="cancelPicture">
+          <i class="fa fa-times"></i>
+        </button>
+      </div>
+    </div>    
     <div class="row">
       <div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
         <form enctype="multipart/form-data" class="form-upload" id="formUpload" onsubmit=${onsubmit}>
+          <a href="#modal1" id="launch" class="waves-effect waves-light btn modal-trigger">
+            <i class="fa fa-camera"></i>
+          </a>
           <div id="fileName" class="fileUpload btn cyan">
-            <span><i class="fa fa-camera"></i> ${translate.message('upload-picture')}</span>
+            <span><i class="fa fa-cloud-upload"></i> ${translate.message('upload-picture')}</span>
             <input name="picture" id="file" type="file" class="upload" onchange=${onchange}/>
           </div>
           <button id="btnUpload" type="submit" class="btn btn-flat cyan hide">${translate.message('upload')}</button>
@@ -34,7 +54,7 @@ module.exports = function (pictures){
     document.getElementById('fileName').classList.toggle('hide');
     document.getElementById('btnUpload').classList.toggle('hide');
     document.getElementById('btnCancel').classList.toggle('hide');
-  }
+  } 
 
   function cancel(){
     toogleButtons();
